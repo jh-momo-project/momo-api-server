b15a2b5df0396310b5ca24ab29042f37c85e5dcd
jihyun <hwiyu25@gmail.com>
2022-08-07 12:07:38 +0900

    :sparkles: linksRepository 주입

--- src/links/links.service.ts
+++ src/links/links.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateLinkDto } from './dto/create-link.dto';
+import { Link } from './entities/link.entity';
 
 @Injectable()
 export class LinksService {
+  constructor(
+    @InjectRepository(Link)
+    private linksRepository: Repository<Link>,
+  ) {}
+
   create(createLinkDto: CreateLinkDto) {
     // create logic
     return createLinkDto;
