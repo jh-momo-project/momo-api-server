6771efcd3d6d8073982fcd342d135e2c13a9e36b
jihyun <hwiyu25@gmail.com>
2022-08-07 12:05:01 +0900

    :sparkles: Postgresql 연결 - LinkEntity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Link } from './links/entities/link.entity';
 import { LinksModule } from './links/links.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jihyunan',
+      password: 'postgres',
+      database: 'momo-db',
+      synchronize: true,
+      entities: [Link],
+    }),
+    LinksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/links/entities/link.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Link {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  url: string;
+
+  @Column({ default: null })
+  title: string;
+
+  @Column({ default: null })
+  description: string;
+
+  @Column({ default: null })
+  thumbnail: string;
+
+  // directory: number; // directoryId
+
+  @Column()
+  order: number;
+}
