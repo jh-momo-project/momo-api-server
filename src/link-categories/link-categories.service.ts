d957c29f20b98fc3d46e2ec10ab23b5088369e69
jihyun <hwiyu25@gmail.com>
2022-08-07 14:25:48 +0900

    :sparkles: LinkCategory Repository 주입

--- src/link-categories/link-categories.service.ts
+++ src/link-categories/link-categories.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateLinkCategoryDto } from './dto/create-link-category.dto';
+import { LinkCategory } from './entities/link-category.entity';
 
 @Injectable()
 export class LinkCategoriesService {
-  create(createLinkCategoryDto: CreateLinkCategoryDto) {
-    return 'This action adds a new linkCategory';
+  constructor(
+    @InjectRepository(LinkCategory)
+    private linkCategoryRepository: Repository<LinkCategory>,
+  ) {}
+
+  async createLinkCategory(createLinkCategoryDto: CreateLinkCategoryDto): Promise<LinkCategory> {
+    const { title } = createLinkCategoryDto;
+    const linkCategory = this.linkCategoryRepository.create({ title });
+    await this.linkCategoryRepository.save(linkCategory);
+    return linkCategory;
   }
 }
